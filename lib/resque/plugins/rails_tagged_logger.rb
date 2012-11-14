d52d4716c8bdab82ec90f830b28a793960b41683
Noah Lorang <noah@datarobots.com>
2012-11-14 08:32:25 -0500

    Allow setting non-JobClassName tags externally

--- lib/resque/plugins/rails_tagged_logger.rb
+++ lib/resque/plugins/rails_tagged_logger.rb
@@ -1,8 +1,10 @@
 module Resque
   module Plugins
     module RailsTaggedLogger
+      mattr_accessor :tags
+      self.tags ||= :resque
       def around_perform_rails_tagged_logger(*args, &block)
-        Rails.logger.tagged :resque, name, &block
+        Rails.logger.tagged tags, name, &block
       end
     end
   end
