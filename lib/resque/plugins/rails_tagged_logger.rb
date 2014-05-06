073b656b47382021af2fd008b5180f5f10c4a453
Noah Lorang <lorang@gmail.com>
2014-05-06 18:34:07 -0400

    Add a uuid to Resque job logs, h.t. @packagethief

--- lib/resque/plugins/rails_tagged_logger.rb
+++ lib/resque/plugins/rails_tagged_logger.rb
@@ -4,8 +4,17 @@ module Resque
       mattr_accessor :tags
       self.tags ||= :resque
       def around_perform_rails_tagged_logger(*args, &block)
-        Rails.logger.tagged tags, name, &block
+        Rails.logger.tagged tags, name, uuid, &block
       end
+
+      def uuid
+        if defined?(SecureRandom.uuid)
+          SecureRandom.uuid
+        else
+          SecureRandom.hex(18)
+        end
+      end
+    
     end
   end
 end
